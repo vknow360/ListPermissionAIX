6a945c52b58cee976431123ab59ec3b3a0da4958
Sunny Gupta <vknow360@gmail.com>
2021-04-04 18:40:27 +0530

    Update classPermissions.java

--- com/jdl/Permission/classPermissions.java
+++ com/jdl/Permission/classPermissions.java
@@ -8,8 +8,8 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import android.view.Window;
 import android.view.WindowManager;
 
